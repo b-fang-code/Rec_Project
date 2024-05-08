a48f3cd8207eb6686163787d7c076be37d879fe6
b-fang <shampuk@gmail.com>
2024-05-08 13:20:15 +0300

    update project

--- recipesproject/recipesapp/admin.py
+++ recipesproject/recipesapp/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 from .models import Recipe, Category, RecipeCategory
 
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'author')
+    list_filter = ('category', 'author')
+    search_fields = ('title', 'category', 'author')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+class RecipeCategoryAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'category')
+    list_filter = ('recipe', 'category')
+    search_fields = ('recipe', 'category')
+
+
+
+
 admin.site.register(Recipe)
 admin.site.register(Category)
 admin.site.register(RecipeCategory)
-
-
-
--- recipesproject/recipesapp/models.py
+++ recipesproject/recipesapp/models.py
@@ -10,14 +10,14 @@ class Category(models.Model):
 
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    instructions = models.TextField()
-    prep_time = models.PositiveIntegerField(help_text='Время приготовления в минутах')
+    title = models.CharField(max_length=200, verbose_name='Название рецепта')
+    description = models.TextField(verbose_name='Описание рецепта')
+    instructions = models.TextField(verbose_name='Инструкция приготовления')
+    prep_time = models.PositiveIntegerField(verbose_name='Время приготовления в минутах')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    categories = models.ManyToManyField(Category, through='RecipeCategory')
-    ingredients = models.TextField()
-    image = models.ImageField(upload_to='recipe_images/', null=True, blank=True)
+    categories = models.ManyToManyField(Category, through='RecipeCategory', verbose_name='Категория')
+    ingredients = models.TextField(verbose_name='Ингредиенты')
+    image = models.ImageField(upload_to='recipe_images/', null=True, blank=True, verbose_name='Изображение')
 
     def __str__(self):
         return self.title
