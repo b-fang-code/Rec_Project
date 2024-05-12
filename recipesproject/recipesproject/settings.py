560976d8460ae1a9c01dc3405ae9ce7ee1fb08b9
b-fang <shampuk@gmail.com>
2024-05-12 17:03:00 +0300

    final pushing

--- recipesproject/recipesproject/settings.py
+++ recipesproject/recipesproject/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,25 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-iydvpb%xloiw%$y%0kfyocu33xj^1ud!q0^28qk3p!-bg3_fze'
 
-if os.environ.get("DEBUG") == "False":
-    DEBUG = False
-else:
-    DEBUG = True
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
-NAME = os.environ.get("NAME")
-USER = os.environ.get("USER")
-PASSWORD = os.environ.get("PASSWORD")
-HOST = os.environ.get("HOST")
-
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'bfrecipes.pythonanywhere.com'
-]
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -137,7 +122,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'recipesapp/static'),
+    os.path.join(BASE_DIR, 'recipesproject/recipesapp/static'),
 )
 
 STATICFILES_FINDERS = (
